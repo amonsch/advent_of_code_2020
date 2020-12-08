8b99685b31af2b2480a5d4cd521ce4c482eeeaae
Andreas Monsch <andreas.monsch@rommelag.com>
2020-12-08 22:30:21 +0100

    Add part two of day 8

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -59,6 +59,55 @@ fn acc_value_before_loop(code: &[(OpCode, i32)]) -> i32 {
     acc
 }
 
+fn acc_value_before_fix(
+    flip_offset: i32,
+    flip_opcode: &OpCode,
+    code: &[(OpCode, i32)],
+) -> Option<i32> {
+    let mut acc = 0;
+    let mut offset = 0;
+    let mut opcode = &code[offset].0;
+    let mut arg = code[offset].1;
+    let mut visited = vec![false; code.len()];
+    loop {
+        visited[offset] = true;
+
+        if offset == flip_offset as usize {
+            opcode = match flip_opcode {
+                OpCode::JMP => &OpCode::NOP,
+                OpCode::NOP => &OpCode::JMP,
+                _ => panic!("Oh noe"),
+            }
+        }
+
+        match opcode {
+            OpCode::NOP => {
+                offset += 1;
+            }
+            OpCode::ACC => {
+                offset += 1;
+                acc += arg;
+            }
+            OpCode::JMP => {
+                offset = (offset as i32 + arg) as usize;
+            }
+        }
+
+        if offset == code.len() {
+            return Some(acc);
+        }
+
+        if visited[offset] {
+            break;
+        }
+
+        opcode = &code[offset].0;
+        arg = code[offset].1;
+    }
+
+    None
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
@@ -68,4 +117,15 @@ fn main() {
 
     let code = input(&args[1]).unwrap();
     println!("ACC before loop: {:?}", acc_value_before_loop(&code));
+
+    for (offset, (opcode, _)) in code.iter().enumerate() {
+        match opcode {
+            OpCode::NOP | OpCode::JMP => {
+                if let Some(result) = acc_value_before_fix(offset as i32, opcode, &code) {
+                    println!("ACC before fix: {:?}", result);
+                }
+            }
+            _ => continue,
+        }
+    }
 }
